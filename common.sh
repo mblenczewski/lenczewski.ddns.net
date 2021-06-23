b017f6734531daa4f905a984d0888a72ff61ce75
Mikołaj Lenczewski <mikolaj.lenczewski308@gmail.com>
2021-06-23 20:45:48 +0100

    Moved all styles to stylesheet

--- common.sh
+++ common.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
+if [ ! -d env ]; then
+	python -m venv env
+
+	. env/bin/activate
+	pip install -r requirements.txt
+	deactivate
+fi
+
 LAYOUTS=layouts
 SCRIPTS=scripts
 TMP=tmp
 OUT=out
 
-[ -d ${TMP} ] || mkdir ${TMP}
-[ -d ${OUT} ] || mkdir ${OUT}
+[ -d $TMP ] || mkdir $TMP
+[ -d $OUT ] || mkdir $OUT
